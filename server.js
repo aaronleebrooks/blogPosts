95afe4888ecfd96bfbf5d9a844621f649657f00f
Aaron Brooks <pleaseshutupaaron@gmail.com>
2017-06-25 00:00:44 -0400

    added the stuff

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const blogPostRouter = require('./blogRouter');
+
+const app = express();
+
+const blogRouter = require('./blogRouter');
+
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.use('/blog-posts', blogPostRouter);
+
+function runServer() {
+  const port = process.env.PORT || 4500;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+
+app.listen(process.env.PORT || 4500, () => {
+	console.log(`Your app is listening on port ${process.env.PORT || 4500}`);
+})
+
+module.exports = {app, runServer, closeServer};
